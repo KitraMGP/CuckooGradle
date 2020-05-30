a0a00fa949bf9b1dffad787d43c4d473e52b77ad
zi-jing <3056894830@qq.com>
2020-05-30 17:52:22 +0800

    Initial commit

---
+++ src/main/java/com/github/zi_jing/cuckoogradle/CuckooGradle.java
@@ -0,0 +1,14 @@
+package com.github.zi_jing.cuckoogradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CuckooGradle implements Plugin<Project> {
+
+	@Override
+	public void apply(Project paramT) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
